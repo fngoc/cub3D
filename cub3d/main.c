40515dd0f03bc2653af0fc8052346c37a964d8da
VitMarKha <vitmarkha@icloud.com>
2020-12-24 22:58:22 +0700

    minor

--- cub3d/main.c
+++ cub3d/main.c
@@ -13,6 +13,6 @@ int	main(int argc, char **argv)
 	lst_map = NULL;
 	checking_errors_arg(argc, argv);
 	parser(argv, &p, lst_map);
-	checking_errors_map(lst_map);
+	// checking_errors_map(lst_map);
 	return (0);
 }
