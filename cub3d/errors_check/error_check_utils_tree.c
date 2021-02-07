416f183b6066c12a66176a31bb0e8b7aa92d9e7c
Vitaliy Khaydarov <vitmarkha@icloud.com>
2021-02-07 13:24:07 +0300

    Update error_check_utils_tree.c

--- cub3d/errors_check/error_check_utils_tree.c
+++ cub3d/errors_check/error_check_utils_tree.c
@@ -16,7 +16,7 @@
 ** check_av_tree: проверка на название '--save'.
 */
 
-int			check_av_tree(char *av)
+int			check_av_save(char *av)
 {
 	if (av[0] == '-' && av[1] == '-' && av[2] == 's' &&
 	av[3] == 'a' && av[4] == 'v' && av[5] == 'e')
@@ -26,29 +26,12 @@ int			check_av_tree(char *av)
 }
 
 /*
-** check_av_two: проверка на название 'map.cub'.
+** check_av_one: проверка на расширение '.cub'.
 */
-
-int			check_av_two(char *av)
+int			check_av_map(char *av)
 {
 	if (av[ft_strlen(av) - 1] == 'b' && av[ft_strlen(av) - 2] == 'u'
-	&& av[ft_strlen(av) - 3] == 'c' && av[ft_strlen(av) - 4] == '.'
-	&& av[ft_strlen(av) - 5] == 'p' && av[ft_strlen(av) - 6] == 'a'
-	&& av[ft_strlen(av) - 7] == 'm')
-		return (0);
-	else
-		return (1);
-}
-
-/*
-** check_av_one: проверка на название 'map.cub'.
-*/
-
-int			check_av_one(char *av)
-{
-	if (av[0] == 'm' && av[1] == 'a' && av[2] == 'p' &&
-	av[3] == '.' && av[4] == 'c' && av[5] == 'u' &&
-	av[6] == 'b')
+	&& av[ft_strlen(av) - 3] == 'c' && av[ft_strlen(av) - 4] == '.')
 		return (0);
 	else
 		return (1);
